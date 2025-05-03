b1efc83ab0b51edcd8621d14a83d94603702d3f2
Shreya Khandelwal <shreyakhandelwal7717@gmail.com>
2025-05-04 02:35:13 +0530

    Changes in register page

--- app/src/main/java/com/example/attendease/RegisterActivity.java
+++ app/src/main/java/com/example/attendease/RegisterActivity.java
@@ -1,4 +1,25 @@
 package com.example.attendease;
 
-public class RegisterActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register); // or your XML filename
+
+        Button getStartedBtn = findViewById(R.id.getStartedBtn);
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch SignUpActivity
+                Intent intent = new Intent(RegisterActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
